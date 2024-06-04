c24dadab29ec75f575ce94b47b8b22110f3a3c0e
shuyx <18271801652@163.com>
2024-06-04 10:03:58 +0800

    up202406041003

--- pushAndBuild.sh
+++ pushAndBuild.sh
@@ -1,31 +1,71 @@
-# 该脚本主要分两个部分。
-# 第一个部分: 提交代码到博客源代码的远程仓库。远程分为gitee和github两个
-# gitee的博客源代码仓库 https://gitee.com/suichentree/suichenblog.git        main分支
-# github的博客源代码仓库 https://github.com/suichentree/suichenblog.git      main分支
+# 脚本说明： pushAndBuild.sh 脚本主要有两个功能。
+# 1.用于将代码提交到本地和远程的博客源代码仓库。
+# 2.对该前端代码进行打包，将打包后的静态文件上传到博客仓库中。
 
-# 第二个部分: 将源代码打包运行，生成博客页面代码后。将页面代码提交到博客页面仓库。分为gitee和github两个
-# gitee的博客页面仓库 https://gitee.com/suichentree/suichentree.git                 main分支
-# github的博客页面仓库 https://github.com/suichentree/suichentree.github.io.git     main分支
+# 博客源代码仓库的远程仓库有两个，一个是gitee,一个是github
+# 博客源代码的gitee远程仓库 https://gitee.com/suichentree/suichenblog.git    main分支   这个远程仓库的仓库别名是 origin ，是默认仓库
+# 博客源代码的github远程仓库 https://github.com/suichentree/suichenblog.git  main分支   这个远程仓库的仓库别名是 github-origin ，非默认仓库
 
-# 注意1: 在git终端中运行脚本。
-# 注意2: 使用 source push.sh 命令运行该文件。bash push.sh 命令无法执行脚本中的cd命令,source push.sh 命令可以。
-# 注意3: git remote -v  命令查询远程仓库
+# 博客仓库的远程仓库有两个，一个是gitee,一个是github
+# 博客仓库的gitee远程仓库  https://gitee.com/suichentree/suichentree.git                 main分支   这个远程仓库的仓库别名是 origin ，是默认仓库
+# 博客仓库的github远程仓库 https://github.com/suichentree/suichentree.github.io.git      main分支   这个远程仓库的仓库别名是 github-origin ，非默认仓库
+
+# 注意1: 需要在git终端中运行脚本。
+# 注意2: 使用 source pushAndBuild.sh 命令运行该文件。bash pushAndBuild.sh 命令无法执行脚本中的cd命令,source pushAndBuild.sh 命令可以。
+# 注意3: 如果需要查询远程仓库地址，可以使用 git remote -v 命令查询远程仓库
 
 
 # 定义commit方法
 function commit() {
     #定义变量commitMessage,并接受外部输入赋值
-    read -p "输入commitMessage: " commitMessage 
-    echo "commitMessage is  $commitMessage"
-    #将暂存区的文件提交到本地分支中
+    read -p "输入 commit 备注: " commitMessage 
+    echo "commit 备注 is  $commitMessage"
+    #将本地暂存区的文件提交到本地分支中
     git commit -m $commitMessage
 }
 
 # 定义push方法
 function push(){
-    # 本地分支推送文件到远程仓库origin的main分支, -f 强制提交
+    # 推送文件到远程仓库origin的main分支
     git push origin main
+    # $?可以获取git push 命令是否运行成功，成功返回0，否则非0。
+    if [ $? -eq 0 ] 
+    then
+        # 输出上传成功消息
+        echo "SUCCESS , git push success"
+    else     
+        # 上传失败，重新执行上传命令
+        echo "ERROR , git push  fail"
+        # 延迟5秒
+        sleep 5s
+        # 重新执行push方法
+        echo "Push Code to Remote Repository Again -------------------"
+        push
+    fi
+
+    # 推送文件到远程仓库github-origin的main分支
     git push github-origin main
+    # $?可以获取git push 命令是否运行成功，成功返回0，否则非0。
+    if [ $? -eq 0 ] 
+    then
+        # 输出上传成功消息
+        echo "SUCCESS , git push success"
+    else     
+        # 上传失败，重新执行上传命令
+        echo "ERROR , git push fail"
+        # 延迟5秒
+        sleep 5s
+        # 重新执行push方法
+        echo "Push Code to Remote Repository Again -------------------"
+        push
+    fi
+
+}
+
+# 定义push方法
+function pushForce(){
+    # 本地分支推送文件到远程仓库origin的main分支, -f 强制提交
+    git push origin main -f
     # $?可以获取git push 命令是否运行成功，成功返回0，否则非0。
     if [ $? -eq 0 ] 
     then
@@ -37,15 +77,10 @@ function push(){
         # 延迟5秒
         sleep 5s
         # 重新执行push方法
-        echo "Push Code to Remote Repository Again -------------------"
-        push
+        echo "PushForce Code to Remote Repository Again -------------------"
+        pushForce
     fi
-}
 
-# 定义push方法
-function pushForce(){
-    # 本地分支推送文件到远程仓库origin的main分支, -f 强制提交
-    git push origin main -f
     git push github-origin main -f
     # $?可以获取git push 命令是否运行成功，成功返回0，否则非0。
     if [ $? -eq 0 ] 
@@ -67,13 +102,13 @@ function pushForce(){
 # 脚本从这里开始--------------
 echo "Start Run push.sh -------------------"
 
-# 第一部分 start--------------------------
+# 第一部分 start --------------------------
 echo "Start Run Part1 -------------------"
 
 # 将所有变动文件添加到暂存区
 git add -A
 
-# 检查是否有文件需要提交
+# 执行 git status 命令检查是否有文件需要提交
 check_commit=`git status`
 if [[ $check_commit =~ "Changes to be committed:" ]] 
 then 
